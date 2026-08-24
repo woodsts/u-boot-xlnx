woodsts/u-boot-xlnx

commit f7cb30ba62c32fe01494522ca73ea73659cae515

fdtdec: Sync fdtdec_get_next_memory_node() with upstream
It is only synchronization with upstream merged version based on commit
3aebac5a759b ("arm64: versal2: Populate DRAM banks before page table size
calculation") and commit 7105f8f2958b ("fdtdec: Add declaration for
get_next_memory_node() helper").

Signed-off-by: Michal Simek <michal.simek@amd.com>
State: upstream (3aebac5a759b)
Message-ID: <2eb7e5fe153709f2aacef2a5bfbff4fd9dc6d97b.1771581189.git.michal.simek@amd.com>

board/amd/versal2/board.c
@@ -406,22 +406,25 @@ int dram_init(void)
 	ofnode mem = ofnode_null();
 	struct resource res;
 	int ret, i, reg = 0;
-	u32 num_banks = 0;
+	u32 num_banks, reloc_use = 0;
 	u64 text = (u64)_start;
 
 	gd->ram_base = (unsigned long)~0;
 
-	mem = get_next_memory_node(mem);
+	mem = fdtdec_get_next_memory_node(mem);
 	if (!ofnode_valid(mem)) {
 		printf("%s: Missing /memory node\n", __func__);
 		return -EINVAL;
 	}
 
+	debug("%s: Text base = 0x%llx\n", __func__, text);
+
 	for (i = 0; i < CONFIG_NR_DRAM_BANKS; i++) {
+		reloc_use = 0;
 		ret = ofnode_read_resource(mem, reg++, &res);
 		if (ret < 0) {
 			reg = 0;
-			mem = get_next_memory_node(mem);
+			mem = fdtdec_get_next_memory_node(mem);
 			if (!ofnode_valid(mem))
 				break;
 
@@ -434,21 +437,23 @@ int dram_init(void)
 			return -EINVAL;
 
 		bank_info[i].phys = (phys_addr_t)res.start;
-		bank_info[i].size  = (phys_size_t)(res.end - res.start + 1);
+		bank_info[i].size = (phys_size_t)(res.end - res.start + 1);
 
 		if (bank_info[i].size == 0)
 			break;
-
-		debug("%s: DRAM Bank #%d: start = 0x%llx, size = 0x%llx\n",
-		      __func__, i, (unsigned long long)bank_info[i].phys,
-		      (unsigned long long)bank_info[i].size);
 
 		if (text >= bank_info[i].phys &&
 		    text < (bank_info[i].phys + bank_info[i].size)) {
 			gd->ram_base = bank_info[i].phys;
 			gd->ram_size = bank_info[i].size;
-			debug("%s: Text base = 0x%llx\n", __func__, text);
+			reloc_use = 1;
 		}
+
+		debug("%s: DRAM Bank #%d: start = 0x%llx, size = 0x%llx %s",
+		      __func__, i, (unsigned long long)bank_info[i].phys,
+		      (unsigned long long)bank_info[i].size,
+		      (reloc_use ? " - USED for RELOCATION\n" : "\n"));
+
 		num_banks++;
 	}
 

arch/arm/mach-versal2/cpu.c
@@ -92,7 +92,7 @@ void mem_map_fill(struct mm_region *bank_info, u32 num_banks)
 }
 
 /**
- * fill_gd_mem_info() - Copy DRAM banks from mem_map to bd_info
+ * fill_bd_mem_info() - Copy DRAM banks from mem_map to bd_info
  *
  * Transfers DRAM bank information from the global versal2_mem_map[]
  * array to bd->bi_dram[] for passing memory configuration to the
@@ -100,10 +100,8 @@ void mem_map_fill(struct mm_region *bank_info, u32 num_banks)
  * address and size are copied.
  *
  * This is called during dram_init_banksize() after the memory map
- * has been populated by mem_map_fill() in dram_init().
- *
- * Context: Called after dram_init() but before kernel handoff
- * Side effects: Modifies bd->bi_dram[] array
+ * has been populated by mem_map_fill() in dram_init(). Called after
+ * dram_init() but before kernel handoff.
  */
 void fill_bd_mem_info(void)
 {

include/fdtdec.h
@@ -200,7 +200,8 @@ struct fdtdec_phandle_args {
 };
 
 /**
- * get_next_memory_node() - Get the next enabled memory node from device tree
+ * fdtdec_get_next_memory_node() - Get the next enabled memory node from device tree
+ *
  * @mem: Current memory node to start search from, or ofnode_null() to get first node
  *
  * This function iterates through device tree nodes with device_type = "memory"
@@ -212,14 +213,14 @@ struct fdtdec_phandle_args {
  * for the next node with the "memory" device_type property and checking
  * its status property.
  *
- * Context: Can be called multiple times to iterate through all memory nodes.
- *          Pass ofnode_null() on first call, then pass the returned node
- *          on subsequent calls until an invalid node is returned.
+ * Can be called multiple times to iterate through all memory nodes.
+ * Pass ofnode_null() on first call, then pass the returned node
+ * on subsequent calls until an invalid node is returned.
  *
  * Return: Next valid, enabled memory ofnode, or invalid ofnode if no more
  *         memory nodes exist
  */
-ofnode get_next_memory_node(ofnode mem);
+ofnode fdtdec_get_next_memory_node(ofnode mem);
 
 /**
  * fdtdec_parse_phandle_with_args() - Find a node pointed by phandle in a list

lib/fdtdec.c
@@ -1083,13 +1083,18 @@ int fdtdec_setup_mem_size_base(void)
 	return 0;
 }
 
-ofnode get_next_memory_node(ofnode mem)
+static ofnode get_next_memory_node(ofnode mem)
 {
 	do {
 		mem = ofnode_by_prop_value(mem, "device_type", "memory", 7);
 	} while (!ofnode_is_enabled(mem));
 
 	return mem;
+}
+
+ofnode fdtdec_get_next_memory_node(ofnode mem)
+{
+	return get_next_memory_node(mem);
 }
 
 int fdtdec_setup_memory_banksize(void)

commit bcb3f13bdd2de18b39705adcfe397f8d16cb0c1e

arm64: versal2: Initialize num_banks variable in dram_init()
In dram_init() num_banks is incremented for every detected DRAM bank
and passed to mem_map_fill() without being initialized, which can add
invalid entries to versal2_mem_map[], so initialize it to zero.

Fixes: f7cb30ba62c3 ("fdtdec: Sync fdtdec_get_next_memory_node() with upstream")
Fixes: 3aebac5a759b ("arm64: versal2: Populate DRAM banks before page table size  calculation")
Signed-off-by: Pranav Tilak <pranav.vinaytilak@amd.com>
Link: https://patch.msgid.link/20260820033422.2967698-1-pranav.vinaytilak@amd.com
Message-ID: <20260819115754.2623184-1-pranav.vinaytilak@amd.com>
State: waiting

board/amd/versal2/board.c
@@ -406,7 +406,8 @@ int dram_init(void)
 	ofnode mem = ofnode_null();
 	struct resource res;
 	int ret, i, reg = 0;
-	u32 num_banks, reloc_use = 0;
+	u32 num_banks = 0;
+	u32 reloc_use = 0;
 	u64 text = (u64)_start;
 
 	gd->ram_base = (unsigned long)~0;